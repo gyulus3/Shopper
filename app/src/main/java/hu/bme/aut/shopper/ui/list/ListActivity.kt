7c2a42464402b1c26eef759d27978c63a614bb2f
Levente Gyulai <LeventeGy@topdesk.com>
2022-04-10 23:04:32 +0200

    Create UI architecture.

---
+++ app/src/main/java/hu/bme/aut/shopper/ui/list/ListActivity.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.shopper.ui.list
+
+import androidx.appcompat.app.AppCompatActivity
+import hu.bme.aut.shopper.ui.list.adapter.ShoppingListItemAdapter
+
+class ListActivity : AppCompatActivity() {
+    lateinit var shoppingListItemAdapter: ShoppingListItemAdapter
+}
---
+++ app/src/main/java/hu/bme/aut/shopper/ui/list/ListViewModel.kt
@@ -0,0 +1,4 @@
+package hu.bme.aut.shopper.ui.list
+
+class ListViewModel {
+}
